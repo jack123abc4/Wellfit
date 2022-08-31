2dace09fde6617a0559a32527f2be11616e5de43
jack123abc4 <jack.r.motherway@gmail.com>
2022-08-31 12:31:47 -0400

    subtract ingredients

--- assets/js/singleResult.js
+++ assets/js/singleResult.js
@@ -10,7 +10,7 @@ var recipeHeader = mainDiv.querySelector("h2");
 var recipeParagraph = mainDiv.querySelector("p");
 var recipeList = mainDiv.querySelector("ul");
 var recipeImage = mainDiv.querySelector("img");
-var recipeObject;
+var myRecipeObject;
 
 var ingredientDiv = document.querySelector("#nutrition");
 var ingredientHeader = ingredientDiv.querySelector("h3");
@@ -22,6 +22,8 @@ backButton.setAttribute("id", "back-btn");
 backButton.textContent = "Back";
 
 var nutrientsToInclude = ["CA", "CHOCDF", "CHOLE", "FAT", "FE", "FIBTG", "K", "NA", "PROCNT", "SUGAR"];
+var nutrientValuesByIng = [0,0,0,0,0,0,0,0,0,0];
+var caloriesByIng = 0;
 
 
 function displaySingleRecipe(params) {
@@ -46,28 +48,55 @@ async function getResults(fullURL,recipeNum) {
         })
         .then(function (data) {
             console.log(data);
-            recipeObject = data.hits[recipeNum].recipe;
-            var recipe = data.hits[recipeNum].recipe;
-            console.log(recipe);
-            console.log(recipe.label);
-            recipeHeader.textContent = recipe.label;
-            recipeImage.setAttribute("src",recipe.image);
-            recipeImage.setAttribute("id","recipe-thumbnail");
-            // recipeParagraph.textContent = "This is the recipe."
-            for (var i = 0; i < recipe.ingredientLines.length; i++) {
-                var recipeLine = document.createElement("li");
-                recipeLine.textContent = recipe.ingredientLines[i];
-                recipeLine.setAttribute("id", "recipe-line-" + i);
-                recipeList.appendChild(recipeLine);
-            }
-            
-            
-            mainDiv.appendChild(backButton);
+            myRecipeObject = data.hits[recipeNum].recipe;
+            refresh();
             return data;
         })
         .catch(function(error) {
             console.log(error);
         });
+}
+
+function refresh() {
+    // var recipe = data.hits[recipeNum].recipe;
+    var recipeLiElements = recipeList.querySelectorAll("li");
+    for (var i = 0; i < recipeLiElements.length; i++) {
+        recipeLiElements[i].remove();
+    }
+    var ingredientLiElements = ingredientDiv.querySelectorAll("li");
+    if (ingredientLiElements) {
+        for (var i = 0; i < ingredientLiElements.length; i++) {
+            ingredientLiElements[i].remove();
+        }
+    }
+    var ingredientBtnElements = ingredientDiv.querySelectorAll("button");
+    if (ingredientBtnElements) {
+        // console.log("Buttons", ingredientBtnElements);
+        for (var i = 0; i < ingredientBtnElements.length; i++) {
+            // console.log(ingredientBtnElements[i]);
+            ingredientBtnElements[i].remove();
+        }
+    }
+    ingredientHeader.textContent = "";
+    
+    
+    
+
+    console.log(myRecipeObject);
+    console.log(myRecipeObject.label);
+    recipeHeader.textContent = myRecipeObject.label;
+    recipeImage.setAttribute("src",myRecipeObject.image);
+    recipeImage.setAttribute("id","recipe-thumbnail");
+    // recipeParagraph.textContent = "This is the recipe."
+    for (var i = 0; i < myRecipeObject.ingredientLines.length; i++) {
+        var recipeLine = document.createElement("li");
+        recipeLine.textContent = myRecipeObject.ingredientLines[i];
+        recipeLine.setAttribute("id", "recipe-line-" + i);
+        recipeList.appendChild(recipeLine);
+    }
+    
+    
+    mainDiv.appendChild(backButton);
 }
 
 async function postIngredient(ingredientObject) {
@@ -97,7 +126,58 @@ async function postIngredient(ingredientObject) {
     return response.json(); // parses JSON response into native JavaScript objects
 }
   
+function subtractIngredient(ingredientText) { 
+    var newIngredients = [];
+    var newIngredientLines = [];
+    for (var i = 0; i < myRecipeObject.ingredientLines.length; i++) {
+        if (myRecipeObject.ingredientLines[i] !== ingredientText) {
+            newIngredientLines.push(myRecipeObject.ingredientLines[i]);
+        }
+    }
+    for (var i = 0; i < myRecipeObject.ingredients.length; i++) {
+        if (myRecipeObject.ingredients[i].text !== ingredientText) {
+            newIngredients.push(myRecipeObject.ingredients[i]);
+        }
+    }
+    myRecipeObject.ingredientLines = newIngredientLines;
+    myRecipeObject.ingredients = newIngredients;
+    updateNutrients();
+    console.log("Updated recipe obj", myRecipeObject);
+    refresh();
 
+}
+
+async function updateNutrients() {
+    // calories, ingredientLines, ingredients, totalNutrients,
+    myRecipeObject.calories = 0;
+    var nutrientKeys = Object.keys(myRecipeObject.totalNutrients);
+    for (var i = 0; i < nutrientKeys.length; i++) {
+        //console.log("This thing", myRecipeObject.totalNutrients[nutrientKeys]);
+        myRecipeObject.totalNutrients[nutrientKeys[i]].quantity = 0;
+    }
+    for (var i = 0; i < myRecipeObject.ingredients.length; i++) {
+        var ingredientObject = myRecipeObject.ingredients[i];
+        postIngredient(ingredientObject)
+        .then((data) => {
+            // console.log("Data", data);
+            myRecipeObject.calories += data.calories;
+            for (var i = 0; i < nutrientKeys.length; i++) {
+                console.log(nutrientKeys[i]);
+                console.log("single ingredient nutrient value", data.totalNutrients[nutrientKeys[i]]);
+                if (data.totalNutrients[nutrientKeys[i]]){
+                    myRecipeObject.totalNutrients[nutrientKeys[i]].quantity += data.totalNutrients[nutrientKeys[i]].quantity;
+                }
+                
+                
+            }
+            console.log(myRecipeObject);
+            //console.log(caloriesByIng,nutrientValuesByIng);
+
+        });
+    }
+    
+
+}
 
 
 function searchTermToURL(searchTerm) {
@@ -113,19 +193,19 @@ backButton.addEventListener("click", function() {
 recipeList.addEventListener("click", function(event){
     // var ingredientNum = event.target.getAttribute("id").split("-")[2];
     var ingredientNum = 0;
-    // console.log(recipeObject.ingredients[ingredientNum].text);
+    // console.log(myRecipeObject.ingredients[ingredientNum].text);
     // console.log(event.target);
-    while (recipeObject.ingredients[ingredientNum].text !== event.target.innerHTML) {
+    while (myRecipeObject.ingredients[ingredientNum].text !== event.target.innerHTML) {
         ingredientNum++;
     }
-    var ingredientObject = recipeObject.ingredients[ingredientNum];
+    var ingredientObject = myRecipeObject.ingredients[ingredientNum];
     console.log(ingredientNum, event.target.textContent);
-    console.log(recipeObject.ingredients[ingredientNum]);
+    console.log(myRecipeObject.ingredients[ingredientNum]);
 
     postIngredient(ingredientObject)
         .then((data) => {
-            console.log(data); // JSON data parsed by `data.json()` call
-            ingredientHeader.textContent = ingredientObject.food;
+            //console.log(data); // JSON data parsed by `data.json()` call
+            ingredientHeader.textContent = ingredientObject.text;
 
             var listItems = ingredientList.querySelectorAll("li");
             for (var i = 0; i < listItems.length; i++) {
@@ -136,31 +216,49 @@ recipeList.addEventListener("click", function(event){
             var caloriesLi = document.createElement("li");
             caloriesLi.textContent = "Calories: " + data.calories;
             ingredientList.appendChild(caloriesLi);
+
+            //caloriesByIng += data.calories;
             console.log(data.totalNutrients);
-            console.log(data.totalNutrients.FAT);
+            //console.log(data.totalNutrients.FAT);
             for (var i = 0; i < nutrientsToInclude.length; i++) {
-                console.log(nutrientsToInclude[i]);
+                //console.log(nutrientsToInclude[i]);
                 var nutrientLi = document.createElement("li");
                 var nutrientValues = data.totalNutrients[nutrientsToInclude[i]];
-                console.log(nutrientValues);
+                //console.log(nutrientValues);
                 nutrientLi.textContent = nutrientValues.label + ": " + nutrientValues.quantity + nutrientValues.unit;
                 ingredientList.appendChild(nutrientLi);
+
+                //nutrientValuesByIng[i] += nutrientValues.quantity;
             }
+            if (!ingredientDiv.querySelector("button")) {
+                var addButton = document.createElement("button");
+                addButton.setAttribute("id", "add-btn");
+                addButton.textContent = "Add";
+                ingredientDiv.appendChild(addButton);
+
+                var subtractButton = document.createElement("button");
+                subtractButton.setAttribute("id", "sub-btn");
+                subtractButton.textContent = "Subtract";
+                ingredientDiv.appendChild(subtractButton);
+            }
+            
+            //console.log(caloriesByIng,nutrientValuesByIng);
+            return data;
 
         });
 })
 
-// postIngredient("https://api.edamam.com/api/food-database/v2/nutrients?app_id=ef193ade&app_key=472b382be6ee874666d1ada17c97d073", {
-//     "ingredients": [
-//       {
-//         "quantity": 750,
-//         "measureURI": "gram",
-//         "foodId": "food_abiw5baauresjmb6xpap2bg3otzu"
-//       }
-//     ]
-//   });
+ingredientDiv.addEventListener("click", function(event) {
+    //console.log(event.target.tagName);
+    if (event.target.tagName === "BUTTON") {
+        // console.log(event.target["id"]);
+        if (event.target["id"] === "add-btn") {
+            //
+        }   
+        else if (event.target["id"] === "sub-btn") {
+            subtractIngredient(ingredientHeader["textContent"]);
+        }
+           
+    }
 
-// postIngredient(null)
-//     .then((data) => {
-//         console.log(data); // JSON data parsed by `data.json()` call
-// });
+})
