9d2ccbf4856813e010de619cf099a4bd5ed97d0f
justin <iinukng@gmail.com>
2022-08-31 13:24:45 -0400

    update.

--- assets/js/ytapi.js
+++ assets/js/ytapi.js
@@ -27,73 +27,69 @@
 
 $(document).ready(function() {
 
-    var YTKEY = "AIzaSyBY1dL8Q9nkhqy5NfIC-4sruEtjLYyoEHU";
-    var search = "";
-    var duration = "any";
-    var filter = "relevance";
-    var maxResults=3
-   
-    $("#duration").change(function () {
-      duration = $(this).children("option:selected").val();
-    });
-    $("#filter").change(function () {
-      filter = $(this).children("option:selected").val();
-    });
-    $("#myForm").submit(function (e) {
-      e.preventDefault();
-   
-      search = $("#search").val();
-   
-      var url = `https://www.googleapis.com/youtube/v3/search?key=${YTKEY}
-          &part=snippet&q=${search}&maxResults=${maxResults}&order=${filter}&videoDuration=${duration}&type=video`;
-   
-      $.ajax({
-        method: "GET",
-        url: url,
-        beforeSend: function () {
-          $("#btn").attr("disabled", true);
-          $("#results").empty();
-        },
-        success: function (data) {
-          console.log(data);
-          $("#btn").attr("disabled", false);
-          displayVideos(data);
-        },
-      });
-    });
-   
-    $("#search").change(function () {
-      search = $("#search").val();
-    });
-   
-    function displayVideos(data) {
-    
-      $("#search").val("");
-   
-      var videoData = "";
-   
-      $("#table").show();
-   
-      data.items.forEach((item) => {
-        videoData = `
-                      <tr>
-                      <td>
-                      <a target="_blank" href="https://www.youtube.com/watch?v=${item.id.videoId}">
-                      ${item.snippet.title}</td>
-                      <td>
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/${data.items[0].id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                      </td>
-                      <td>
-                      <a target="_blank" href="https://www.youtube.com/channel/${item.snippet.channelId}">${item.snippet.channelTitle}</a>
-                      </td>
-                      </tr
-                      `;
-   
-        $("#results").append(videoData);
-      });
-    }
-  });
-
+  var YTKEY = "AIzaSyBY1dL8Q9nkhqy5NfIC-4sruEtjLYyoEHU";
+  var search = "";
+  var duration = "any";
+  var filter = "relevance";
+  var maxResults=3
  
-
-
+  $("#duration").change(function () {
+    duration = $(this).children("option:selected").val();
+  });
+  $("#filter").change(function () {
+    filter = $(this).children("option:selected").val();
+  });
+  $("#myForm").submit(function (e) {
+    e.preventDefault();
+ 
+    search = $("#search").val();
+ 
+    var url = `https://www.googleapis.com/youtube/v3/search?key=${YTKEY}
+        &part=snippet&q=${search}&maxResults=${maxResults}&order=${filter}&videoDuration=${duration}&type=video`;
+ 
+    $.ajax({
+      method: "GET",
+      url: url,
+      beforeSend: function () {
+        $("#btn").attr("disabled", true);
+        $("#results").empty();
+      },
+      success: function (data) {
+        console.log(data);
+        $("#btn").attr("disabled", false);
+        displayVideos(data);
+      },
+    });
+  });
+ 
+  $("#search").change(function () {
+    search = $("#search").val();
+  });
+ 
+  function displayVideos(data) {
+  
+    $("#search").val("");
+ 
+    var videoData = "";
+ 
+    $("#table").show();
+ 
+    data.items.forEach((item) => {
+      videoData = `
+                    <tr>
+                    <td>
+                    <a target="_blank" href="https://www.youtube.com/watch?v=${item.id.videoId}">
+                    ${item.snippet.title}</td>
+                    <td>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </td>
+                    <td>
+                    <a target="_blank" href="https://www.youtube.com/channel/${item.snippet.channelId}">${item.snippet.channelTitle}</a>
+                    </td>
+                    </tr
+                    `;
+ 
+      $("#results").append(videoData);
+    });
+  }
+});
